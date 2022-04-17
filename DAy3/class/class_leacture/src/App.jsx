e35f2e7cab5541fd2c74043821e0348379d409b9
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-04-18 01:24:08 +0530

    Classpractise_Day3

--- DAy3/class/class_leacture/src/App.jsx
+++ DAy3/class/class_leacture/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+// import { useState } from 'react'
+// import logo from './logo.svg'
+import { Category } from '../../components/category';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const Navbar = [
+   {image: "https://rukminim2.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100",title: "Top Offer"},
+ {image: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",title: "Grocery"},
+ {image: "https://rukminim2.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100",title: "Mobile"},
+ {image: "https://rukminim2.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100",title: "Fashion"},
+ {image: "https://rukminim2.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",title: "Electronics"},
+];
   return (
-  <div className="App">
-    <h1>Test</h1>
-  </div>
+  <div className='nav'>
+   {Navbar.map(el => <Category image={el.image}label={el.title}/>)}
+    </div>
   )
 }
 
-export default App
+export default App;
