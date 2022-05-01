5535f2ac7c0d23e9d6888643dd91956fddb985d0
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-05-01 15:40:28 +0530

    commit-Simple_todo

---
+++ todo-ass/simple-todo/src/components/Todo.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { ToDoList } from "./TodoList"
+import {nanoid} from "nanoid";
+import "./Todo.css"
+
+const Todo = () => {
+    const [TodoData, setTodoData] = useState([]);
+    const getData = (id) => {
+        const data = document.getElementById(id).value;
+        document.getElementById(id).value = "";
+        setTodoData([...TodoData,
+            {
+                title: data,
+                status: false,
+                id: nanoid(5)
+            }
+        ]);
+    }
+    const toggle = (id) => {
+        setTodoData([
+            ...TodoData.map((e) => {
+                return (e.id === id ? {...e, status: !e.status} : e)
+                // if (e.id === id) {
+                //     e.status = "Done"
+                // }
+                // return e;
+            })
+            // ...TodoData.map((e) =>
+            // (e.id === id ? {...e, status: "Done"} : e))
+        ])
+    }
+
+    return (
+        <>
+            <ToDoList data={TodoData} toggle={toggle}/>
+            <div id="input-div">
+                <input id="user-input" type="text" placeholder="Write Something"></input>
+                <button onClick={() => {getData("user-input")}}>+</button>
+            </div>
+        </>
+    )
+}
+
+export { Todo }
