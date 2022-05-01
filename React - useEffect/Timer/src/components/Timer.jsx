2c4b30b34c3103f97341d370a91b6714e37a8d0f
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-05-01 19:55:45 +0530

    comit-timer

---
+++ React - useEffect/Timer/src/components/Timer.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+export const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    let id = setInterval(() => {
+      setSeconds(seconds + 1);
+      if (seconds === 59) {
+        setMinutes(minutes + 1);
+        setSeconds(0);
+      }
+     
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  });
+// }
+
+  return (
+    <>
+      <h1>Timer</h1>
+      <h2>
+        {minutes < 10 ? "0" + minutes : minutes} :{" "}
+        {seconds < 10 ? "0" + seconds : seconds}
+      </h2>
+      <span>
+        Start Time: <input type="number" />
+      </span>
+          End Time: <input type="number" />
+          <button onClick={() => { start() }}>Start</button>
+    </>
+  );
+};
